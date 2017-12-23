4550a6c5f2268b777e1b842a15de8bb90bac72a6
Wendsly <tripmineweasel@hotmail.com>
2017-12-23 16:39:44 +0100

    DifferentButWorkingCameraFollow

---
+++ Assets/Scripts/DifferentCameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifferentCameraFollow : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject target;
+
+	[SerializeField]
+	private float lerp = 0.2f;
+
+
+	void FixedUpdate () {
+		Debug.Assert (target != null);
+		Vector3 position = Vector3.Lerp (transform.position, target.transform.position, lerp);
+		position.z = -20;
+		transform.position = position;
+	}
+}
